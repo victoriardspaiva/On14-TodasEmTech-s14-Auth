95531c7e12a12a5812849e2212a8f9b58253290e
Victoria Paiva <victoriardspaiva@recife.pe.senac.br>
2021-11-17 21:14:58 -0300

    comandos em aula

--- src/controller/colaboradorasController.js
+++ src/controller/colaboradorasController.js
@@ -1,29 +1,43 @@
 const colaboradoras = require("../models/colaboradoras")
-
+const SECRET = process.env.SECRET
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 
 const getAll = (req, res) => {
+  const authHearder = req.get('authorization')
+  const token = authHearder.split(' ')[1];
   console.log(req.url);
-    colaboradoras.find(function (err, colaboradoras){
+  console.log(token);
+
+  if (!token) {
+    return res.status(401).send("Erro no header")
+  }
+
+  jwt.verify(token, SECRET, function (erro) {
+    if (erro) {
+      return res.status(403).send('Não autorizado!')
+    }
+
+    colaboradoras.find(function (err, colaboradoras) {
       res.status(200).send(colaboradoras)
-    })     
-};
+    })
+  })
+}
 
 const postColaboradora = (req, res) => {
-  console.log(req.body);
+  const senhaHash = bcrypt.hashSync(req.body.password, 10)
+  req.body.password = senhaHash
 
   let colaboradora = new colaboradoras(req.body);
-    colaboradora.save(function(err){
+  colaboradora.save(function (err) {
     if (err) res.status(500).send({ message: err.message })
 
     res.status(201).send(colaboradora.toJSON());
   })
 };
 
-
-
-
 module.exports = {
-    getAll,
-    postColaboradora,
+  getAll,
+  postColaboradora,
 }
